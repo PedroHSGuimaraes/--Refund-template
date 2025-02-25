b2d73d782411d7c0968ff7dbe7aa2dceb81fc072
Pedro Henrique Santos Guimarães <pedro.guimaraes02@hotmail.com>
2025-02-24 21:07:54 -0300

    Feat :V1

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const amount = document.querySelector("#amount");
 const expense = document.querySelector("#expense");
 const category = document.querySelector("#category");
 
+const expenseList = document.querySelector("ul");
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
 
@@ -33,7 +34,24 @@ function formatCurrencyBRL(value) {
 
 function expenseAdd(newExpense) {
   try {
-    
+    const expenseItem = document.createElement("li");
+    const expenseInco = document.createElement("img");
+    const expenseInfo = document.createElement("div");
+    const expenseName = document.createElement("strong");
+    const expenseCategory = document.createElement("span");
+
+    expenseItem.classList.add("expense");
+    expenseInfo.classList.add("expense-info");
+
+    expenseName.textContent = newExpense.expense;
+    expenseCategory.textContent = newExpense.categoryName;
+
+    expenseInco.setAttribute("src", `./img/${newExpense.categoryId}.svg`);
+    expenseInco.setAttribute("alt", newExpense.categoryName);
+
+    expenseInfo.append(expenseName,expenseCategory);
+    expenseItem.append(expenseInco, expenseInfo);
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Ocorreu um erro ao adicionar a despesa");
     console.log(" expenseAdd ➜ error:", error);
